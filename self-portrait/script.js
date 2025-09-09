0924fe0da03b72fb1eed7ed95a880ff073398bd1
dalia <dalia.thornburgh@gmail.com>
2025-09-08 23:24:54 -0600

    self portrait

--- self-portrait/script.js
+++ self-portrait/script.js
@@ -3,23 +3,85 @@ const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var width = 150;
-var height = 35;
-
 //2. Definir si es 2D o 3D
 const ctx = canvas.getContext("2d");
 
-//draw background
-ctx.fillStyle = "rgb(0, 0, 0)"; 
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+//the rest of the hair
+ctx.beginPath();
+ctx.strokeStyle = " #000000";
+ctx.fillStyle = "#000000";
+ctx.rect(canvas.width / 2 - 160, canvas.height / 2 - 20, 320, 150);
+ctx.stroke();
+ctx.fill();
 
-//draw middle circle 
+//face base 
 
 ctx.beginPath();
-ctx.strokeStyle = "white";
+ctx.strokeStyle = " #fff2e6";
+ctx.fillStyle = "#fff2e6";
 ctx.lineWidth = 3;
-ctx.arc(canvas.width / 2, canvas.height / 2, width, 0, Math.PI * 2);
+ctx.arc(canvas.width / 2, canvas.height / 2, 150, 0, Math.PI * 2);
 ctx.stroke();
+ctx.fill();
+
+
+//ears
+ctx.beginPath();
+ctx.strokeStyle = " #fff2e6";
+ctx.fillStyle = "#fff2e6";
+ctx.arc(canvas.width / 2 - 150, canvas.height / 2, 40, 0, Math.PI * 2);
+ctx.stroke();
+ctx.fill();
+ctx.beginPath();
+ctx.arc(canvas.width / 2 + 150, canvas.height / 2, 40, 0, Math.PI * 2);
+ctx.stroke();
+ctx.fill();
+
+//left eye
+ctx.beginPath();
+ctx.strokeStyle = " #000000";
+ctx.fillStyle = "#000000";
+ctx.arc(canvas.width / 2 - 75, canvas.height / 2, 10, 0, Math.PI * 2);
+ctx.stroke();
+ctx.fill();
+
+//right eye
+ctx.beginPath();
+ctx.strokeStyle = " #000000";
+ctx.fillStyle = "#000000";
+ctx.arc(canvas.width / 2 + 75, canvas.height / 2, 10, 0, Math.PI * 2);
+ctx.stroke();
+ctx.fill();
+
+//mouth
+ctx.beginPath();
+ctx.strokeStyle = " #ff9999";
+ctx.fillStyle = "#ff9999";
+ctx.arc(canvas.width / 2, canvas.height / 2 + 50, 50, 0, Math.PI);
+ctx.stroke();
+ctx.fill();
+
+
+//hair
+ctx.beginPath();
+ctx.strokeStyle = "rgb(0, 0, 0)";
+ctx.fillStyle = "black";
+ctx.arc(canvas.width / 2, canvas.height / 2 - 20, 160, Math.PI, 0);
+ctx.stroke();
+ctx.fill();
+
+//hair strands
+ctx.beginPath();
+ctx.arc(canvas.width / 2 - 185, canvas.height / 2 + 10, 50, 200, Math.PI * 2.5);
+ctx.stroke();
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(canvas.width / 2 + 160, canvas.height / 2 + 10, 50, 45, Math.PI * 1.5);
+ctx.stroke();
+ctx.fill();
+
+
 
 
 
