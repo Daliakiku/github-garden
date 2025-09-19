85016ee69d41a8c09a18401aa439d7d9129144dd
dalia <dalia.thornburgh@gmail.com>
2025-09-18 23:33:05 -0600

    sep 18 exersize

--- sep-18/script.js
+++ sep-18/script.js
@@ -11,16 +11,43 @@ console.log(canvas);
 //create scene
 const scene = new THREE.Scene();
 
-//object/mesh = geometry + material
-const geometry = new THREE.BoxGeometry(1, 1, 1); //width, height, depth
-const material = new THREE.MeshBasicMaterial({ color: "red"});
-const mesh = new THREE.Mesh(geometry, material);
+//create object group
+const group = new THREE.Group();
+scene.add(group);
+group.position.y = -1;
+group.position.z = -7;
 
-//add mesh to scene
-scene.add(mesh);
+//create trunk
+const trunk = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 3, 1),
+    new THREE.MeshBasicMaterial({ color: '#573630' })
+);
+group.add(trunk);
+trunk.position.y = -1;
 
-//rotate mesh
-mesh.rotation.y = 0.5;
+const leaves1 = new THREE.Mesh(
+    new THREE.SphereGeometry(2, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+group.add(leaves1);
+leaves1.position.y = 2;
+
+const leaves2 = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+group.add(leaves2);
+leaves2.position.x = 1.5;
+leaves2.position.y = 3;
+
+const leaves3 = new THREE.Mesh(
+    new THREE.SphereGeometry(1.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+group.add(leaves3);
+leaves3.position.x = -1;
+leaves3.position.y = 3;
+
 
 //camera
 const sizes = { //creating an object to store sizes
@@ -31,7 +58,21 @@ const sizes = { //creating an object to store sizes
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height); //fov, aspect ratio
 //add camera to the scene
 scene.add(camera);
-camera.position.z = 3; //move the camera away from the cube
+//camera.position.z = 6; //move the camera away from the cube
+
+//zoom in button
+document.querySelector('.zoom-in').addEventListener('click', () => {
+    camera.position.z -= 5;
+    console.log(camera.position.z);
+});
+
+
+//zoom out button
+document.querySelector('.zoom-out').addEventListener('click', () => {
+    camera.position.z += 5;
+    console.log(camera.position.z);
+});
+
 
 //renderer
 const renderer = new THREE.WebGLRenderer({
